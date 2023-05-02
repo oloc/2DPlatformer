905964694d2845995a51353ac7687c3df8364064
Olivier Locard <develocard@gmail.com>
2023-05-02 17:55:41 +0200

    Add camera sharpness adjustement

--- Assets/Scripts/CameraMovements.cs
+++ Assets/Scripts/CameraMovements.cs
@@ -2,14 +2,18 @@ using UnityEngine;
 
 public class CameraMovements : MonoBehaviour
 {
+    [Header("Objects")]
     [SerializeField]
     public Transform player;
+    [Header("Parameters")]
+    [SerializeField]
+    private float _followSharpness;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 currentPosition = transform.position;
         Vector3 nextPosition = player.transform.position + new Vector3(0, 1, -10);
-        transform.position = Vector3.Lerp(currentPosition, nextPosition, 5);
+        transform.position = Vector3.Lerp(currentPosition, nextPosition, _followSharpness);
     }
 }
