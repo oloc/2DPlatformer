667885d1bc1b9705474dc1339b867757f2c8ac85
Olivier Locard <develocard@gmail.com>
2023-05-04 17:38:35 +0200

    Add Win to the Exit Door

--- Assets/Scripts/ExitDoor.cs
+++ Assets/Scripts/ExitDoor.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using TMPro;
 
 public class ExitDoor : MonoBehaviour
 {
     [SerializeField] private GameValues _currentValues;
+    [SerializeField] private GameObject _winTextObject;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
             _currentValues.score += 100;
+            _winTextObject.SetActive(true);
             Debug.Log("Win the game!");
         }
     }
