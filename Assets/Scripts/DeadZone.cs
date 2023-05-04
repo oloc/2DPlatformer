c6478a6c263446297206520cc99a092765cdf4dc
Olivier Locard <develocard@gmail.com>
2023-05-04 16:24:57 +0200

    Add Deadzone and restart

---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+
+    [SerializeField] private GameValues _defaultValues;
+    [SerializeField] private Transform _playerTransform;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Collider2D>().CompareTag("Player"))
+        {
+            _playerTransform.position = _defaultValues.playerPosition;
+        }
+    }
+
+}
