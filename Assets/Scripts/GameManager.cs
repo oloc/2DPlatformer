d327c14d06db62f5f4451b260fd9cf5974148c0f
Olivier Locard <develocard@gmail.com>
2023-05-04 16:22:21 +0200

    Set Player position in GameManager Awake

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,12 +3,15 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameValues _defaultValues;
     [SerializeField] private GameValues _currentValues;
     [SerializeField] private Text _scoreText;
+    [SerializeField] private Transform _playerTransform;
 
     private void Awake()
     {
         _currentValues.score = 0;
+        _playerTransform.position = _defaultValues.playerPosition;
     }
 
     private void Update()
