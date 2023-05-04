d874d71fb1ac9a9a364b40ab7003855e9a3f2eea
Olivier Locard <develocard@gmail.com>
2023-05-04 15:40:19 +0200

    Add GameValues, current and default

---
+++ Assets/Scripts/GameValues.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New GameValues", menuName = "Custom/GameValues")]
+public class GameValues : ScriptableObject
+{
+    public int score;
+    public int alePoints;
+    public int muttonPoints;
+}
