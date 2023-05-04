13d2a4f09796619c9353865e390f2e0746206689
Olivier Locard <develocard@gmail.com>
2023-05-04 16:07:10 +0200

    Points in the item prefab

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -4,12 +4,13 @@ public class Item : MonoBehaviour
 {
 
     [SerializeField] private GameValues gameValues;
+    [SerializeField] private int points;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<Collider2D>().CompareTag("Player"))
         {
-            gameValues.score ++;
+            gameValues.score += points;
             Destroy(gameObject);
         }
     }
