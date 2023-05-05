548ac0d9500dba149f242f433bab5f30e1eb7ca8
Olivier Locard <develocard@gmail.com>
2023-05-05 15:04:23 +0200

    Add checkpoints

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+
+    [SerializeField] GameValues _defaultValues;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag(Player.playerTag))
+        {
+            _defaultValues.playerPosition = transform.position;
+        }
+    }
+}
