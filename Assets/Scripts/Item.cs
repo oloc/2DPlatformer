6eb9b24b640a6fda74b56f7b67462f70d7c4d47b
Olivier Locard <develocard@gmail.com>
2023-05-05 10:30:39 +0200

    BSR: variable naming

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
 
-    [SerializeField] private GameValues gameValues;
-    [SerializeField] private int points;
+    [SerializeField] private GameValues _gameValues;
+    [SerializeField] private int _itemPoints;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<Collider2D>().CompareTag("Player"))
         {
-            gameValues.score += points;
+            _gameValues.score += _itemPoints;
             Destroy(gameObject);
         }
     }
