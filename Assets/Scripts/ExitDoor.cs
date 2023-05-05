92d3d790ef4e914b658faca38194092b29bc173d
Olivier Locard <develocard@gmail.com>
2023-05-05 15:27:55 +0200

    Add quit game after 3 seconds

--- Assets/Scripts/ExitDoor.cs
+++ Assets/Scripts/ExitDoor.cs
@@ -1,9 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 public class ExitDoor : MonoBehaviour
 {
     [SerializeField] private GameValues _currentValues;
     [SerializeField] private GameObject _winTextObject;
+    [SerializeField] private float _waitBeforeClose;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -11,6 +13,17 @@ public class ExitDoor : MonoBehaviour
         {
             _currentValues.score += 100;
             _winTextObject.SetActive(true);
+            StartCoroutine(Close(_waitBeforeClose));
         }
+    }
+
+    private IEnumerator Close(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
